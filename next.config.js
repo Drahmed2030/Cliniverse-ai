0a8c71b26596e8acb94a335dda69c2ce67646529
Drahmed2030 <ahmyohmed121@gmail.com>
2026-07-23 15:01:23 +0300

    fix: no cache + build id

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  generateBuildId: async () => {
+    return Date.now().toString()
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  headers: async () => [{
+    source: "/(.*)",
+    headers: [{
+      key: "Cache-Control",
+      value: "no-cache, no-store, must-revalidate"
+    }]
+  }]
 }
 module.exports = nextConfig
