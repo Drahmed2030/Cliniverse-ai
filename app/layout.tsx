25435c6657c5e4ebfaaefb383a1a1941ff59bb1b
Drahmed2030 <ahmyohmed121@gmail.com>
2026-07-24 09:35:38 +0300

    fix: remove ErrorBoundary from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
-        <ErrorBoundary><ThemeProvider>{children}</ThemeProvider></ErrorBoundary>
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   )
