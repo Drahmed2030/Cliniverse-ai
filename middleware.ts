7472b9f5f122bf8512d989515351402231a3276a
Drahmed2030 <ahmyohmed121@gmail.com>
2026-07-26 02:09:00 +0300

    fix: add pass-through middleware to fix MIDDLEWARE_INVOCATION_FAILED

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,10 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const response = NextResponse.next()
-  response.headers.set('Cache-Control', 'no-cache, no-store, must-revalidate')
-  response.headers.set('Pragma', 'no-cache')
-  response.headers.set('Expires', '0')
-  return response
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/(.*)',
-}
+  matcher: [],
+};
