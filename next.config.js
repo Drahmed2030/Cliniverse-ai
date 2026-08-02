3fab39669f48ef9582e7387b9da99b8b97da2414
Drahmed2030 <ahmyohmed121@gmail.com>
2026-08-02 17:40:43 +0300

    fix: add static export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {
+const nextConfig = {n  output: "export",
   typescript: { ignoreBuildErrors: true },
   generateBuildId: async () => {
     return Date.now().toString()
